b01ae378a2bec31944df5a0119d09cf6634642da
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2020-12-29 20:30:10 +0100

    Solve day 1 part 2

--- day-1/report-repair/Program.fs
+++ day-1/report-repair/Program.fs
@@ -1,20 +1,40 @@
 open System
 open System.IO;
 
-let findEntriesWithSum sum entries =
-    let withoutIndex index = Seq.indexed >> Seq.filter (fun (i, _) -> i <> index) >> Seq.map (fun (_, x) -> x)
-    let pairs = entries |> Seq.indexed |> Seq.collect (fun (i, x) -> entries |> withoutIndex i |> Seq.map (fun y -> x, y) )
+let findTwoEntriesWithSum sum entries =
+    let indexedEntries = entries |> Seq.indexed
+    let pairs = seq {
+        for (ix, x) in indexedEntries do
+        for (iy, y) in indexedEntries |> Seq.skip (ix + 1) do
+        x, y
+    }
     pairs |> Seq.tryFind (fun (x, y) -> x + y = sum)
+
+let findThreeEntriesWithSum sum entries =
+    let indexedEntries = entries |> Seq.indexed
+    let triples = seq {
+        for (ix, x) in indexedEntries do
+        for (iy, y) in indexedEntries |> Seq.skip (ix + 1) do
+        for (iz, z) in indexedEntries |> Seq.skip (iy + 1) do
+        x, y, z
+    }
+    triples |> Seq.tryFind (fun (x, y, z) -> x + y + z = sum)
 
 [<EntryPoint>]
 let main argv =
     let inputPath = Path.Combine(Environment.CurrentDirectory, "input.txt")
     let entries = inputPath |> File.ReadAllLines |> Seq.map int
-    let sum = 2020
-    let result = entries |> findEntriesWithSum sum
 
-    match result with
+    let sum = 2020
+    let resultForTwo = entries |> findTwoEntriesWithSum sum
+    let resultForThree = entries |> findThreeEntriesWithSum sum
+
+    match resultForTwo with
       | Some (x, y) -> printfn "%d + %d = %d. %d * %d = %d" x y sum x y (x * y)
-      | _ -> printfn "No matching entries found."
+      | _ -> printfn "No two entries found that have a sum of %d." sum
+
+    match resultForThree with
+      | Some (x, y, z) -> printfn "%d + %d + %d = %d. %d * %d * %d = %d" x y z sum x y z (x * y * z)
+      | _ -> printfn "No three entries found that have a sum of %d." sum
 
     0 // return an integer exit code
