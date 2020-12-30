76ee7ef58ec2e2524fd304e623796a7f77e308ed
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2020-12-30 12:19:27 +0100

    Refactor report repair and add tests

---
+++ day-1/ReportRepair.Tests/Program.Tests.fs
@@ -0,0 +1,34 @@
+module ReportRepair.Tests
+
+open NUnit.Framework
+open Program
+
+[<Test>]
+let ``No entries are returned if sequence is empty`` () =
+    let actual = findEntriesWithSum 100 2 []
+    Assert.AreEqual(None, actual)
+
+[<Test>]
+let ``No entries are returned if sequence contains one element that is not equal to the sum`` () =
+    let actual = findEntriesWithSum 100 2 [90]
+    Assert.AreEqual(None, actual)
+
+[<Test>]
+let ``No entries are returned if sequence contains one element that is equal to the sum`` () =
+    let actual = findEntriesWithSum 100 2 [100]
+    Assert.AreEqual(None, actual)
+
+[<Test>]
+let ``No entries are returned if sequence contains two elements and one is half of the sum`` () =
+    let actual = findEntriesWithSum 100 2 [50; 60]
+    Assert.AreEqual(None, actual)
+
+[<Test>]
+let ``Returns the two entries that sum up to the sum`` () =
+    let actual = findEntriesWithSum 100 2 [10; 20; 30; 40; 50; 60]
+    Assert.AreEqual(Some [40; 60], actual)
+
+[<Test>]
+let ``Returns the three entries that sum up to the sum`` () =
+    let actual = findEntriesWithSum 100 3 [10; 20; 30; 40; 50; 60]
+    Assert.AreEqual(Some [10; 30; 60], actual)
---
+++ day-1/ReportRepair/Program.fs
@@ -0,0 +1,27 @@
+open System
+open System.IO;
+
+let rec getCombinations n l =
+    match n, l with
+    | 0, _ -> [[]]
+    | _, [] -> []
+    | k, (x::xs) -> List.map ((@) [x]) (getCombinations (k-1) xs) @ getCombinations k xs
+
+let findEntriesWithSum sum count entries =
+    entries |> Seq.toList |> getCombinations count |> Seq.tryFind (fun x -> Seq.sum x = sum)
+
+let printResult sum count result =
+    let product = Seq.fold (( * )) 1
+    match result with
+      | Some x -> printfn "Sum of %A = %d. Product of %A = %d" x sum x (x |> product)
+      | _ -> printfn "No %d entries found that have a sum of %d." count sum
+
+[<EntryPoint>]
+let main argv =
+    let inputPath = Path.Combine(Environment.CurrentDirectory, "input.txt")
+    let entries = inputPath |> File.ReadAllLines |> Seq.map int
+
+    entries |> findEntriesWithSum 2020 2 |> printResult 2020 3
+    entries |> findEntriesWithSum 2020 3 |> printResult 2020 3
+
+    0 // return an integer exit code
