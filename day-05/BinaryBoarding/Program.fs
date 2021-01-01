e6c6848d1ca36c931d875e6358a7edb51c4b4190
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-01 11:48:08 +0100

    Solve day 5 part 2

--- day-05/BinaryBoarding/Program.fs
+++ day-05/BinaryBoarding/Program.fs
@@ -39,16 +39,35 @@ let decodeSeat (code: string): Seat =
       Column = column
       SeatId = seatId }
 
+let findEmptySeatId (seats: Seat seq): int =
+    let seatIds = seats |> Seq.map (fun x -> x.SeatId)
+    let minSeatId = seatIds |> Seq.min
+    let maxSeatId = seatIds |> Seq.max
+    let seatIdSet = seatIds |> Set.ofSeq
+
+    let isSeatTaken id = seatIdSet |> Set.contains id
+    let isSeatFree id = not <| isSeatTaken id
+
+    [ minSeatId .. maxSeatId ]
+    |> Seq.find
+        (fun id ->
+            isSeatFree id
+            && isSeatTaken (id - 1)
+            && isSeatTaken (id + 1))
+
 [<EntryPoint>]
 let main argv =
     let inputPath = "./input.txt"
     let codes = File.ReadAllLines inputPath
 
+    let seats = codes |> Seq.map decodeSeat
+
     let highestSeatId =
-        codes
-        |> Seq.map (decodeSeat >> (fun x -> x.SeatId))
-        |> Seq.max
+        seats |> Seq.map (fun x -> x.SeatId) |> Seq.max
+
+    let emptySeatId = seats |> findEmptySeatId
 
     printfn "The highest SeatId is %d" highestSeatId
+    printfn "The SeatId of the empty seat id %d" emptySeatId
 
     0
