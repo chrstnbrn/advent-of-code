1ed85e74af09e04a0affab12b65e75d7c7f9095d
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-02 11:34:40 +0100

    Solve day 8 part 1

--- day-08/HandheldHalting.Tests/Program.Tests.fs
+++ day-08/HandheldHalting.Tests/Program.Tests.fs
@@ -1,11 +1,22 @@
 module HandheldHalting.Tests
 
 open NUnit.Framework
+open Program
 
-[<SetUp>]
-let Setup () =
-    ()
+let instructions =
+    [ "nop +0"
+      "acc +1"
+      "jmp +4"
+      "acc +3"
+      "jmp -3"
+      "acc -99"
+      "acc +1"
+      "jmp -4"
+      "acc +6" ]
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let getValueBeforeRepeatingInstruction () =
+    let actual =
+        getValueBeforeRepeatingInstruction instructions
+
+    Assert.AreEqual(5, actual)
--- day-08/HandheldHalting/Program.fs
+++ day-08/HandheldHalting/Program.fs
@@ -1,13 +1,66 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open System.IO
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Operation =
+    | Nop
+    | Acc
+    | Jmp
+
+type Instruction = { Operation: Operation; Argument: int }
+
+let getOperation (s: string): Operation =
+    match s with
+    | "nop" -> Nop
+    | "acc" -> Acc
+    | "jmp" -> Jmp
+    | _ -> raise (ArgumentException("Invalid operation: " + s))
+
+let getInstruction (instructionText: string): Instruction =
+    let parts = instructionText.Split(' ')
+    let operation = parts.[0] |> getOperation
+    let argument = parts.[1] |> int
+
+    { Operation = operation
+      Argument = argument }
+
+let getNextIndex (instructions: Instruction list) (currentIndex: int): int =
+    let currentInstruction = instructions.[currentIndex]
+
+    match currentInstruction.Operation with
+    | Jmp -> currentIndex + currentInstruction.Argument
+    | _ -> currentIndex + 1
+
+let rec getInstructionsBeforeLoop (instructions: Instruction list) (visitedIndexes: int list): Instruction seq =
+    let currentIndex = visitedIndexes |> Seq.last
+    let nextIndex = getNextIndex instructions currentIndex
+
+    seq {
+        yield instructions.[currentIndex]
+
+        if (not <| Seq.contains nextIndex visitedIndexes) then
+            yield! getInstructionsBeforeLoop instructions (visitedIndexes @ [ nextIndex ])
+    }
+
+let getValueBeforeRepeatingInstruction (instructionTexts: string seq): int =
+    let instructions =
+        instructionTexts
+        |> Seq.map getInstruction
+        |> Seq.toList
+
+    let instructionsBeforeLoop =
+        getInstructionsBeforeLoop instructions [ 0 ]
+
+    instructionsBeforeLoop
+    |> Seq.filter (fun x -> x.Operation = Acc)
+    |> Seq.sumBy (fun x -> x.Argument)
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let instructions = "./input.txt" |> File.ReadAllLines
+
+    let value =
+        getValueBeforeRepeatingInstruction instructions
+
+    printfn "The value before any instruction is run a second time is %d" value
+
     0 // return an integer exit code
