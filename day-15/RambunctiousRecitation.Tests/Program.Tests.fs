5d5a897eecbdc539fa367318cd51d78483542131
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-06 22:06:29 +0100

    Solve day 15 part 1

--- day-15/RambunctiousRecitation.Tests/Program.Tests.fs
+++ day-15/RambunctiousRecitation.Tests/Program.Tests.fs
@@ -1,11 +1,27 @@
 module RambunctiousRecitation.Tests
 
 open NUnit.Framework
+open Program
 
-[<SetUp>]
-let Setup () =
-    ()
+let getNthSpokenNumberTestCases =
+    [ TestCaseData([ 0; 3; 6 ], 1).Returns(0)
+      TestCaseData([ 0; 3; 6 ], 2).Returns(3)
+      TestCaseData([ 0; 3; 6 ], 3).Returns(6)
+      TestCaseData([ 0; 3; 6 ], 4).Returns(0)
+      TestCaseData([ 0; 3; 6 ], 5).Returns(3)
+      TestCaseData([ 0; 3; 6 ], 6).Returns(3)
+      TestCaseData([ 0; 3; 6 ], 7).Returns(1)
+      TestCaseData([ 0; 3; 6 ], 8).Returns(0)
+      TestCaseData([ 0; 3; 6 ], 9).Returns(4)
+      TestCaseData([ 0; 3; 6 ], 10).Returns(0)
+      TestCaseData([ 1; 2; 2 ], 4).Returns(1)
+      TestCaseData([ 1; 3; 2 ], 2020).Returns(1)
+      TestCaseData([ 2; 1; 3 ], 2020).Returns(10)
+      TestCaseData([ 1; 2; 3 ], 2020).Returns(27)
+      TestCaseData([ 2; 3; 1 ], 2020).Returns(78)
+      TestCaseData([ 3; 2; 1 ], 2020).Returns(438)
+      TestCaseData([ 3; 1; 2 ], 2020).Returns(1836) ]
 
-[<Test>]
-let Test1 () =
-    Assert.Pass()
+
+[<TestCaseSource(nameof getNthSpokenNumberTestCases)>]
+let getNthSpokenNumber startingNumbers n = getNthSpokenNumber n startingNumbers
--- day-15/RambunctiousRecitation/Program.fs
+++ day-15/RambunctiousRecitation/Program.fs
@@ -1,13 +1,38 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+let rec getNumbers spokenNumbers previousTurn previousNumber =
+    let secondToLastTurnSpoken =
+        spokenNumbers |> Map.tryFind previousNumber
 
-open System
+    let number =
+        secondToLastTurnSpoken
+        |> Option.map (fun n -> previousTurn - n)
+        |> Option.defaultValue 0
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+    let newSpokenNumbers =
+        spokenNumbers
+        |> Map.add previousNumber previousTurn
+
+    seq {
+        yield number
+        yield! getNumbers newSpokenNumbers (previousTurn + 1) number
+    }
+
+let getNthSpokenNumber (n: int) (startingNumbers: int list) =
+    let initialNumbers =
+        Map(
+            startingNumbers
+            |> Seq.take (startingNumbers.Length - 1)
+            |> Seq.mapi (fun i x -> (x, i + 1))
+        )
+
+    getNumbers initialNumbers startingNumbers.Length (List.last startingNumbers)
+    |> Seq.append startingNumbers
+    |> Seq.item (n - 1)
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let startingNumbers = [ 1; 0; 15; 2; 10; 13 ]
+
+    let number = getNthSpokenNumber 2020 startingNumbers
+    printfn "The 2020th number spoken is %d" number
+
+    0
