6b305ce1fbf3e94f85b704f13725b14128ee0d7b
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-06 16:20:51 +0100

    Solve day 13 part 2

--- day-13/ShuttleSearch.Tests/Program.Tests.fs
+++ day-13/ShuttleSearch.Tests/Program.Tests.fs
@@ -8,3 +8,17 @@ open Program
 let getResult () =
     let actual = getResult "939" "7,13,x,x,59,x,31,19"
     Assert.AreEqual(295, actual)
+
+let getTimestampTestData =
+    [ TestCaseData("10,11,x,13,14").Returns(10I)
+      TestCaseData("7,13,x,x,59,x,31,19")
+          .Returns(1068781I)
+      TestCaseData("17,x,13,19").Returns(3417I)
+      TestCaseData("67,7,59,61").Returns(754018I)
+      TestCaseData("67,x,7,59,61").Returns(779210I)
+      TestCaseData("67,7,x,59,61").Returns(1261476I)
+      TestCaseData("1789,37,47,1889")
+          .Returns(1202161486I) ]
+
+[<TestCaseSource(nameof getTimestampTestData)>]
+let getTimeStamp busIdsText = getTimestamp busIdsText
--- day-13/ShuttleSearch/Program.fs
+++ day-13/ShuttleSearch/Program.fs
@@ -1,3 +1,4 @@
+open System
 open System.IO
 
 let getSchedule busId = Seq.initInfinite (fun i -> i * busId)
@@ -23,6 +24,46 @@ let getResult (arrivalTimeText: string) (busIdsText: string): int =
 
     busId * waitTime
 
+let rec greatestCommonDivisor a b =
+    if b = 0I then
+        abs a
+    else
+        greatestCommonDivisor b (a % b)
+
+let leastCommonMultiple a b = a * b / (greatestCommonDivisor a b)
+
+type Constraint = { BusId: bigint; Offset: bigint }
+
+let getConstraints (busIdsText: string) =
+    busIdsText.Split ','
+    |> Array.indexed
+    |> Array.filter (fun (_, x) -> x <> "x")
+    |> Array.map
+        (fun (i, busId) ->
+            { BusId = bigint (int busId)
+              Offset = bigint (i) })
+
+let departsAt time busId = time % busId = 0I
+
+let getNext (timestamp, period) busConstraint =
+    let nextTimestamp =
+        Seq.initInfinite (fun i -> timestamp + bigint (i) * period)
+        |> Seq.find (fun t -> departsAt (t + busConstraint.Offset) busConstraint.BusId)
+
+    let nextPeriod =
+        leastCommonMultiple period busConstraint.BusId
+
+    (nextTimestamp, nextPeriod)
+
+let getTimestamp (busIdsText: string): bigint =
+    let constraints = getConstraints busIdsText
+    let firstBusId = constraints.[0].BusId
+    let initialTimestamp, initialPeriod = firstBusId, firstBusId
+
+    constraints
+    |> Seq.fold getNext (initialTimestamp, initialPeriod)
+    |> fst
+
 [<EntryPoint>]
 let main argv =
     let input = "./input.txt" |> File.ReadAllLines
@@ -31,7 +72,9 @@ let main argv =
     let busIds = input.[1]
 
     let result = getResult startTime busIds
+    printfn "Part 1: The result is %d" result
 
-    printfn "The result is %d" result
+    let timestamp = getTimestamp busIds
+    printfn "Part 2: The timestamp is %A" timestamp
 
     0
