412247dc9378dcae1e93c7a767a9654ed944eec2
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-06 20:20:44 +0100

    Solve day 14 part 2

--- day-14/DockingData.Tests/Program.Tests.fs
+++ day-14/DockingData.Tests/Program.Tests.fs
@@ -3,14 +3,30 @@ module DockingData.Tests
 open NUnit.Framework
 open Program
 
-[<Test>]
-let getSum () =
-    let program =
-        [| "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
-           "mem[8] = 11"
-           "mem[7] = 101"
-           "mem[8] = 0" |]
+module Part1 =
 
-    let actual = getSum program
+    [<Test>]
+    let getSum () =
+        let program =
+            [| "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+               "mem[8] = 11"
+               "mem[7] = 101"
+               "mem[8] = 0" |]
 
-    Assert.AreEqual(165, actual)
+        let actual = Part1.getSum program
+
+        Assert.AreEqual(165, actual)
+
+module Part2 =
+
+    [<Test>]
+    let getSum () =
+        let program =
+            [| "mask = 000000000000000000000000000000X1001X"
+               "mem[42] = 100"
+               "mask = 00000000000000000000000000000000X0XX"
+               "mem[26] = 1" |]
+
+        let actual = Part2.getSum program
+
+        Assert.AreEqual(208, actual)
--- day-14/DockingData/Program.fs
+++ day-14/DockingData/Program.fs
@@ -4,9 +4,9 @@ open System.IO
 open System.Text.RegularExpressions
 
 type BitmaskBit =
-    | OverwriteWithZero
-    | OverwriteWithOne
-    | Unchanged
+    | Zero
+    | One
+    | X
 
 type Bitmask = BitmaskBit array
 
@@ -22,11 +22,23 @@ type State =
     { Memory: Map<uint64, uint64>
       Bitmask: Bitmask }
 
+module UInt64 =
+    let getBits (value: uint64) =
+        value
+        |> BitConverter.GetBytes
+        |> BitArray
+        |> Seq.cast<bool>
+
+    let fromBitArray (bitArray: BitArray): uint64 =
+        let mutable bytes = Array.zeroCreate 8
+        bitArray.CopyTo(bytes, 0)
+        BitConverter.ToUInt64(bytes, 0)
+
 let parseBitmaskBit =
     function
-    | '0' -> OverwriteWithZero
-    | '1' -> OverwriteWithOne
-    | 'X' -> Unchanged
+    | '0' -> Zero
+    | '1' -> One
+    | 'X' -> X
     | ch -> failwithf "Invalid bitmask char: %c" ch
 
 let parseUpdateBitmask text =
@@ -71,64 +83,112 @@ let parseInstruction text: Instruction =
     | None, Some i -> i
     | _ -> failwithf "Invalid instruction %s" text
 
-let bitArrayToSeq (bitArray: BitArray) =
-    seq {
-        for i = 0 to bitArray.Length - 1 do
-            yield bitArray.Get(i)
-    }
-
-let bitArrayToUInt64 (bitArray: BitArray): uint64 =
-    let mutable bytes = Array.zeroCreate 8
-    bitArray.CopyTo(bytes, 0)
-    BitConverter.ToUInt64(bytes, 0)
-
-let getBit bit =
-    function
-    | OverwriteWithZero -> false
-    | OverwriteWithOne -> true
-    | Unchanged -> bit
-
-let getValue (value: uint64) (bitmask: Bitmask): uint64 =
-    let bitArray =
-        value
-        |> BitConverter.GetBytes
-        |> BitArray
-        |> bitArrayToSeq
-
-    bitmask
-    |> Seq.rev
-    |> Seq.zip bitArray
-    |> Seq.map (fun (bit, bitmaskBit) -> getBit bit bitmaskBit)
-    |> Seq.toArray
-    |> BitArray
-    |> bitArrayToUInt64
-
-let getMemory state writeValue =
-    let newValue = getValue writeValue.Value state.Bitmask
-    Map.add writeValue.MemoryAddress newValue state.Memory
-
-let getNextState state instruction =
+let getNextState decoder state instruction =
     match instruction with
     | UpdateBitmask bitmask -> { state with Bitmask = bitmask }
-    | WriteValue w ->
-        { state with
-              Memory = getMemory state w }
+    | WriteValue w -> { state with Memory = decoder state w }
 
-let run instructions =
+let run decoder instructions =
     let initialState = { Memory = Map.empty; Bitmask = [||] }
-    instructions |> Seq.fold getNextState initialState
 
-let getSum (program: string array) =
-    let finalState =
-        program |> Array.map parseInstruction |> run
+    instructions
+    |> Seq.fold (getNextState decoder) initialState
 
-    finalState.Memory |> Map.toSeq |> Seq.sumBy snd
+let sumOfAllValues = Map.toSeq >> Seq.sumBy snd
+
+module Part1 =
+    let getBit bit =
+        function
+        | Zero -> false
+        | One -> true
+        | X -> bit
+
+    let getValue (value: uint64) (bitmask: Bitmask): uint64 =
+        let bits = UInt64.getBits value
+
+        bitmask
+        |> Seq.rev
+        |> Seq.zip bits
+        |> Seq.map (fun (bit, bitmaskBit) -> getBit bit bitmaskBit)
+        |> Seq.toArray
+        |> BitArray
+        |> UInt64.fromBitArray
+
+    let decoder state writeValue =
+        let newValue = getValue writeValue.Value state.Bitmask
+        Map.add writeValue.MemoryAddress newValue state.Memory
+
+    let getSum (program: string array): uint64 =
+        let finalState =
+            program
+            |> Array.map parseInstruction
+            |> run decoder
+
+        finalState.Memory |> sumOfAllValues
+
+module Part2 =
+    type AddressBit =
+        | Zero
+        | One
+        | Floating
+
+    let getAddressBit bit =
+        function
+        | BitmaskBit.Zero -> if bit then One else Zero
+        | BitmaskBit.One -> One
+        | BitmaskBit.X -> Floating
+
+    let replaceAtIndex index value =
+        Array.mapi (fun i x -> if i = index then value else x)
+
+    let rec getAddressesForTemplate template =
+        seq {
+            if Array.contains Floating template then
+                let index =
+                    template |> Array.findIndex ((=) Floating)
+
+                for value in [ One; Zero ] do
+                    let newTemplate = template |> replaceAtIndex index value
+                    yield! getAddressesForTemplate newTemplate
+            else
+                yield
+                    template
+                    |> Array.map ((=) One)
+                    |> BitArray
+                    |> UInt64.fromBitArray
+        }
+
+    let getAddresses (writeValue: WriteValue) (bitmask: Bitmask): uint64 seq =
+        let bits = UInt64.getBits writeValue.MemoryAddress
+
+        bitmask
+        |> Seq.rev
+        |> Seq.zip bits
+        |> Seq.map (fun (bit, bitmaskBit) -> getAddressBit bit bitmaskBit)
+        |> Seq.toArray
+        |> getAddressesForTemplate
+
+    let decoder state writeValue =
+        let addresses = getAddresses writeValue state.Bitmask
+        let setValue map address = Map.add address writeValue.Value map
+        addresses |> Seq.fold setValue state.Memory
+
+    let getSum (program: string array): uint64 =
+        let finalState =
+            program
+            |> Array.map parseInstruction
+            |> run decoder
+
+        finalState.Memory |> sumOfAllValues
 
 [<EntryPoint>]
 let main argv =
     let program = File.ReadAllLines "./input.txt"
-    let sum = getSum program
 
-    printfn "The sum of all values is %d" sum
+    let sum1 = Part1.getSum program
+    printfn "Part1: The sum of all values is %d" sum1
+
+    let sum2 = Part2.getSum program
+    printfn "Part2: The sum of all values is %d" sum2
 
     0
