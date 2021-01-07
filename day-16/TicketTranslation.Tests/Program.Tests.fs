63f04f85d059f4933718e1475807a5b004f9fbe2
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-07 22:13:06 +0100

    Solve day 16 part 1

--- day-16/TicketTranslation.Tests/Program.Tests.fs
+++ day-16/TicketTranslation.Tests/Program.Tests.fs
@@ -1,11 +1,21 @@
 module TicketTranslation.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
+open Program
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let getTicketScanningErrorRate () =
+    let rules =
+        [| "class: 1-3 or 5-7"
+           "row: 6-11 or 33-44"
+           "seat: 13-40 or 45-50" |]
+
+    let tickets =
+        [| "7,3,47"
+           "40,4,50"
+           "55,2,20"
+           "38,6,12" |]
+
+    let actual = getTicketScanningErrorRate tickets rules
+
+    Assert.AreEqual(71, actual)
--- day-16/TicketTranslation/Program.fs
+++ day-16/TicketTranslation/Program.fs
@@ -1,13 +1,70 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open System.IO
+open System.Text.RegularExpressions
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Range = { Start: int; End: int }
+
+type Rule =
+    { Field: string
+      ValidRanges: Range [] }
+
+type Ticket = int []
+
+let parseRule ruleText =
+    let fieldPattern = "^(?<Field>\w+):"
+    let rangePattern = "(?<Start>\d+)-(?<End>\d+)"
+
+    let field =
+        Regex.Match(ruleText, fieldPattern).Groups.["Field"]
+            .Value
+
+    let ranges =
+        Regex.Matches(ruleText, rangePattern)
+        |> Seq.map
+            (fun m ->
+                { Start = int m.Groups.["Start"].Value
+                  End = int m.Groups.["End"].Value })
+        |> Seq.toArray
+
+    { Field = field; ValidRanges = ranges }
+
+let getTicketValues (ticketText: string) = ticketText.Split(',') |> Array.map int
+
+let isInRange value range =
+    range.Start <= value && value <= range.End
+
+let isValidValue value rule =
+    rule.ValidRanges |> Seq.exists (isInRange value)
+
+let isInvalidValue value rule = rule |> isValidValue value |> not
+
+let getTicketScanningErrorRate ticketTexts ruleTexts =
+    let rules = ruleTexts |> Array.map parseRule
+
+    let ticketValues =
+        ticketTexts |> Array.collect getTicketValues
+
+    let invalidTicketValues =
+        ticketValues
+        |> Seq.filter (fun v -> rules |> Array.forall (isInvalidValue v))
+
+    invalidTicketValues |> Seq.sum
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let text = File.ReadAllText "./input.txt"
+
+    let blocks =
+        text.Split(Environment.NewLine + Environment.NewLine)
+
+    let rules = blocks.[0].Split(Environment.NewLine)
+
+    let tickets =
+        blocks.[2].Split(Environment.NewLine)
+        |> Array.filter (String.IsNullOrWhiteSpace >> not)
+        |> Array.skip 1
+
+    let ticketScanningErrorRate = getTicketScanningErrorRate tickets rules
+    printfn "The ticket scanning error rate is %d" ticketScanningErrorRate
+
+    0
