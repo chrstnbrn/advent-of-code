fa502be6b67502a6aa3e4089be70edef0f8f0877
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-09 16:39:21 +0100

    Solve day 17 part 1

--- day-17/ConwayCubes.Tests/Program.Tests.fs
+++ day-17/ConwayCubes.Tests/Program.Tests.fs
@@ -1,11 +1,76 @@
 module ConwayCubes.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
+open Program
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let getNumberOfActiveCubes () =
+    let cycles = 6
+    let initialState = [| ".#."; "..#"; "###" |]
+
+    let actual =
+        getNumberOfActiveCubes cycles initialState
+
+    Assert.AreEqual(112, actual)
+
+[<Test>]
+let getNeighborCoordinates () =
+    let cubeCoordinate = (1, 2, 3)
+
+    let actual = getNeighborCoordinates cubeCoordinate
+
+    let expected =
+        [ (0, 1, 2)
+          (0, 1, 3)
+          (0, 1, 4)
+          (0, 2, 2)
+          (0, 2, 3)
+          (0, 2, 4)
+          (0, 3, 2)
+          (0, 3, 3)
+          (0, 3, 4)
+          (1, 1, 2)
+          (1, 1, 3)
+          (1, 1, 4)
+          (1, 2, 2)
+          (1, 2, 4)
+          (1, 3, 2)
+          (1, 3, 3)
+          (1, 3, 4)
+          (2, 1, 2)
+          (2, 1, 3)
+          (2, 1, 4)
+          (2, 2, 2)
+          (2, 2, 3)
+          (2, 2, 4)
+          (2, 3, 2)
+          (2, 3, 3)
+          (2, 3, 4) ]
+
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let simulateCycle () =
+    let activeCubes =
+        Set [ (1, 0, 0)
+              (2, 1, 0)
+              (0, 2, 0)
+              (1, 2, 0)
+              (2, 2, 0) ]
+
+    let actual = simulateCycle activeCubes
+
+    let expected =
+        Set [ (0, 1, -1)
+              (2, 2, -1)
+              (1, 3, -1)
+              (0, 1, 0)
+              (2, 1, 0)
+              (1, 2, 0)
+              (2, 2, 0)
+              (1, 3, 0)
+              (0, 1, 1)
+              (2, 2, 1)
+              (1, 3, 1) ]
+
+    Assert.AreEqual(expected, actual)
--- day-17/ConwayCubes/Program.fs
+++ day-17/ConwayCubes/Program.fs
@@ -1,13 +1,74 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open System.IO
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type CubeState =
+    | Active
+    | Inactive
+
+let getNeighborCoordinates (x, y, z) =
+    seq {
+        for i in [ x - 1 .. x + 1 ] do
+            for j in [ y - 1 .. y + 1 ] do
+                for k in [ z - 1 .. z + 1 ] do
+                    if (i, j, k) <> (x, y, z) then
+                        yield (i, j, k)
+    }
+    |> Set
+
+let getNextCubeState coordinate activeCoordinates =
+    let state =
+        if Set.contains coordinate activeCoordinates then
+            Active
+        else
+            Inactive
+
+    let activeNeighbors =
+        coordinate
+        |> getNeighborCoordinates
+        |> Set.intersect activeCoordinates
+        |> Seq.length
+
+    match (state, activeNeighbors) with
+    | (Active, 2)
+    | (_, 3) -> Active
+    | _ -> Inactive
+
+let toCubeState =
+    function
+    | '.' -> Inactive
+    | '#' -> Active
+    | c -> failwithf "Invalid CubeState %c" c
+
+let simulateCycle activeCubes =
+    activeCubes
+    |> Seq.collect getNeighborCoordinates
+    |> Set
+    |> Set.union activeCubes
+    |> Set.filter (fun c -> getNextCubeState c activeCubes = Active)
+
+let getActiveCubes (initialState: string array) =
+    seq {
+        for x in [ 0 .. initialState.[0].Length - 1 ] do
+            for y in [ 0 .. initialState.Length - 1 ] do
+                let state = initialState.[y].[x] |> toCubeState
+                if (state = Active) then yield (x, y, 0)
+    }
+    |> Set
+
+let getNumberOfActiveCubes (cycles: int) (initialState: string array): int =
+    let initiallyActiveCubes = getActiveCubes initialState
+
+    let finalState =
+        [ 1 .. cycles ]
+        |> Seq.fold (fun s _ -> simulateCycle s) initiallyActiveCubes
+
+    finalState |> Seq.length
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let initialState = File.ReadAllLines "./input.txt"
+
+    let numberofActiveCubes = getNumberOfActiveCubes 6 initialState
+    printfn "After the sixth cycle %d cubes are left in the active state" numberofActiveCubes
+
+    0
