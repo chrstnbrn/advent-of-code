4996771adadd78248f203fd2abf526aa43446fa4
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-10 14:27:37 +0100

    Solve day 19 part 1

--- day-19/MonsterMessages.Tests/Program.Tests.fs
+++ day-19/MonsterMessages.Tests/Program.Tests.fs
@@ -1,11 +1,26 @@
 module MonsterMessages.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
+open Program
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let getMessagesMatchingRule () =
+    let rules =
+        [| "0: 4 1 5"
+           "1: 2 3 | 3 2"
+           "2: 4 4 | 5 5"
+           "3: 4 5 | 5 4"
+           "4: \"a\""
+           "5: \"b\"" |]
+
+    let messages =
+        [| "ababbb"
+           "bababa"
+           "abbbab"
+           "aaabbb"
+           "aaaabbb" |]
+
+    let actual = getMessagesMatchingRule rules messages 0
+
+    let expected = [| "ababbb"; "abbbab" |]
+    Assert.AreEqual(expected, actual)
--- day-19/MonsterMessages/Program.fs
+++ day-19/MonsterMessages/Program.fs
@@ -1,13 +1,94 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open System.IO
+open System.Text.RegularExpressions
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Rule =
+    { Number: int
+      Definition: RuleDefinition }
+
+and RuleDefinition =
+    | MatchCharacter of char
+    | MatchAnySubRule of SubRule []
+
+and SubRule = int []
+
+let parseSubRules (s: string) =
+    s.Split('|')
+    |> Array.map (
+        (fun x ->
+            x.Split()
+            |> Array.filter (not << String.IsNullOrWhiteSpace)
+            |> Array.map int)
+    )
+
+let parseRule ruleText =
+    let pattern =
+        "^(?<Number>\d+):\s*((\"(?<Character>\w)\")|(?<Subrules>(\d+\s*)+(\|(\s*\d+)+)*))$"
+
+    let m = Regex.Match(ruleText, pattern)
+    let number = int m.Groups.["Number"].Value
+
+    if (m.Groups.["Character"].Success) then
+        let character = m.Groups.["Character"].Value |> char
+
+        { Number = number
+          Definition = MatchCharacter character }
+    else
+        let subRules =
+            m.Groups.["Subrules"].Value |> parseSubRules
+
+        { Number = number
+          Definition = MatchAnySubRule subRules }
+
+let rec cartesianProduct lists =
+    match lists with
+    | [] -> Seq.singleton []
+    | L :: Ls ->
+        seq {
+            for x in L do
+                for xs in cartesianProduct Ls -> x :: xs
+        }
+
+let rec getValidMessages rules ruleNumber =
+    let rule = rules |> Map.find ruleNumber
+
+    let getValidMessagesForSubRule =
+        Array.map (getValidMessages rules)
+        >> Array.toList
+        >> cartesianProduct
+        >> Seq.map (String.concat "")
+        >> Seq.toArray
+
+    match rule.Definition with
+    | MatchCharacter ch -> [| ch.ToString() |]
+    | MatchAnySubRule subRules ->
+        subRules
+        |> Array.collect getValidMessagesForSubRule
+
+let getMessagesMatchingRule ruleTexts messages ruleNumber =
+    let rules =
+        ruleTexts
+        |> Array.map (parseRule >> (fun rule -> (rule.Number, rule)))
+        |> Map
+
+    let validMessages = getValidMessages rules ruleNumber
+
+    Set.intersect (Set messages) (Set validMessages)
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let input = File.ReadAllText "./input.txt"
+
+    let blocks =
+        input.Split(Environment.NewLine + Environment.NewLine)
+        |> Array.map (fun x -> x.Split(Environment.NewLine))
+
+    let rules, messages = blocks.[0], blocks.[1]
+
+    let numberofMessages =
+        getMessagesMatchingRule rules messages 0
+        |> Seq.length
+
+    printfn "%d messages completely match rule 0" numberofMessages
+
+    0
