d3af0337eefa688e152d5d2821e254609518939e
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-18 19:06:29 +0100

    Solve day 22 part 1

--- day-22/CrabCombat.Tests/Program.Tests.fs
+++ day-22/CrabCombat.Tests/Program.Tests.fs
@@ -1,11 +1,13 @@
 module CrabCombat.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
+open Program
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let getScore () =
+    let player1Deck = [ 9; 2; 6; 3; 1 ]
+    let player2Deck = [ 5; 8; 4; 7; 10 ]
+
+    let actual = getScore player1Deck player2Deck
+
+    Assert.AreEqual(306, actual)
--- day-22/CrabCombat/Program.fs
+++ day-22/CrabCombat/Program.fs
@@ -1,13 +1,70 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System.IO
 
-open System
+type GameState =
+    { Player1Deck: Deck
+      Player2Deck: Deck }
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+and Deck = int list
+
+let getWinningDeck =
+    function
+    | { Player1Deck = []; Player2Deck = deck } -> deck
+    | { Player1Deck = deck; Player2Deck = [] } -> deck
+    | _ -> failwith "The game is not over yet"
+
+let isGameOver state =
+    List.isEmpty state.Player1Deck
+    || List.isEmpty state.Player2Deck
+
+let playCard deck = (List.head deck, List.tail deck)
+
+let rec play state =
+    if (isGameOver state) then
+        state
+    else
+        let (player1Card, player1Deck) = playCard state.Player1Deck
+        let (player2Card, player2Deck) = playCard state.Player2Deck
+
+        let player1Wins = player1Card > player2Card
+
+        let newState =
+            if player1Wins then
+                { Player1Deck = player1Deck @ [ player1Card; player2Card ]
+                  Player2Deck = player2Deck }
+            else
+                { Player1Deck = player1Deck
+                  Player2Deck = player2Deck @ [ player2Card; player1Card ] }
+
+        play newState
+
+let calculateScore =
+    getWinningDeck
+    >> List.rev
+    >> List.mapi (fun i card -> (i + 1) * card)
+    >> List.sum
+
+let getScore player1Deck player2Deck =
+    let initialState =
+        { Player1Deck = player1Deck
+          Player2Deck = player2Deck }
+
+    initialState |> play |> calculateScore
+
+let getDeck title =
+    Array.skipWhile ((<>) title)
+    >> Array.skip 1
+    >> Array.takeWhile ((<>) "")
+    >> Array.map int
+    >> Array.toList
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let fileContent = File.ReadAllLines "./input.txt"
+
+    let player1Deck = getDeck "Player 1:" fileContent
+    let player2Deck = getDeck "Player 2:" fileContent
+
+    let score = getScore player1Deck player2Deck
+    printfn "The winning player's score is %d" score
+
+    0
