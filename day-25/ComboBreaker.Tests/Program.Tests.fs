2caf967605da0c6e55b0c765e67f8a16555c9389
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-01-24 19:25:05 +0100

    Solve day 25

--- day-25/ComboBreaker.Tests/Program.Tests.fs
+++ day-25/ComboBreaker.Tests/Program.Tests.fs
@@ -1,11 +1,14 @@
 module ComboBreaker.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
+open Program
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let getEncryptionKey () =
+    let cardPublicKey = 5764801L
+    let doorPublicKey = 17807724L
+
+    let actual =
+        getEncryptionKey cardPublicKey doorPublicKey
+
+    Assert.AreEqual(14897079L, actual)
--- day-25/ComboBreaker/Program.fs
+++ day-25/ComboBreaker/Program.fs
@@ -1,13 +1,28 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let transformOnce subjectNumber value = value * subjectNumber % 20201227L
+
+let transform subjectNumber loopSize =
+    [ 1 .. loopSize ]
+    |> Seq.fold (fun value _ -> transformOnce subjectNumber value) 1L
+
+let getLoopSize publicKey =
+    Seq.initInfinite ((+) 1)
+    |> Seq.scan (fun (_, value) loopSize -> (loopSize, transformOnce 7L value)) (0, 1L)
+    |> Seq.find (snd >> (=) publicKey)
+    |> fst
+
+let getEncryptionKey cardPublicKey doorPublicKey =
+    cardPublicKey
+    |> getLoopSize
+    |> transform doorPublicKey
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let cardPublicKey = 1526110L
+    let doorPublicKey = 20175123L
+
+    getEncryptionKey cardPublicKey doorPublicKey
+    |> printfn "The encryption key is %d"
+
+    0
